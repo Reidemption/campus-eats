cd5363ef8bb6ce4182e27d5d6f44c6b96e1e7931
minhducnguyen26 <minhducnguyen260101@gmail.com>
2021-07-12 23:40:46 -0600

    Added color palette

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app_wrapper">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+:root {
+  --red     : hsl(358, 74%, 42%);
+  --red-dark: hsl(358, 73%, 31%);
+  --gray    : hsl(355, 7%, 65%);
+  --navy    : hsl(212, 68%, 15%);
 }
 </style>
