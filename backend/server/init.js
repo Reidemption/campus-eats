8ae2a0fb729c8003a4f3dd815741cbd3ad639f3f
d00415496 <d00415496@dmail.dixie.edu>
2021-07-15 11:24:07 -0600

    commit data access layer files

--- backend/server/init.js
+++ backend/server/init.js
@@ -1,13 +1,14 @@
-const server = require("./services");
-const dataAccess = require("../dataAccessLayer/DAO");
+const services = require("./services");
+const connectDBX = require("../dataAccessLayer/DAO");
 
 // define a port
 const port =process.argv[2]||process.env.port||7777;
 
-dataAccess.connectDB(()=>{
+connectDBX(()=>{
     console.log("-> Succesfully connect to DB");
-    server.service.listen(port,()=>{
+    services.listen(port,()=>{
         console.log(`-> The CAMPUSEAT server is running at port:${port}`);
         console.log(`--------------------------------------------------`);
     });
 });
+
--- backend/server/services.js
+++ backend/server/services.js
@@ -1,3 +1,43 @@
+const express = require("express")
 const cors = require("cors")
-var {mysql, connectDB} = require("../dataAccessLayer/DAO")
-console.log("")
+// const url = require("url")
+
+const services = express();
+// ========== Middlewares ===========
+services.use(cors())
+services.use(express)
+services.use(express.json({}))
+// services.use(express.static("../../frontend/"))
+
+// ======== Request handlers =========
+// --- LOG ---
+services.use((req,res,next)=>{
+    res.setHeader("Content-Type","application/json");
+    console.log("======================== REQUEST ==========================")
+    console.log(
+        "- Time:",Date.now(),
+        "- Method:", req.method,
+        "- Url:", req.url,
+        "- Body:", req.body
+    ),
+    next()
+})
+
+// --- PATH DIRECTORIES ---
+// services.use((req,res,next)=>{
+//     console.log(
+//         "- Path:", url(req.url).pathname
+//     ),
+//     next()
+// })
+
+services.get("/hello",(req,res)=>{
+    console.log(`${req.headers}`)
+    console.log(`hi!`)
+    res.status(200).json({message:`hi!`})
+    return;
+})
+
+
+// ========= EXPORT MODULE ==========
+module.exports=services
