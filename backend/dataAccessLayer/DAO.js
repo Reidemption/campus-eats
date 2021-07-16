3f1a4c935af9cd5e3955e4ba897be642f77ba0e9
d00415496 <d00415496@dmail.dixie.edu>
2021-07-16 04:24:19 -0600

    commit to test

--- backend/dataAccessLayer/DAO.js
+++ backend/dataAccessLayer/DAO.js
@@ -7,7 +7,7 @@ function connectDB(callbackFunction){
     // let connectionString = ``;
     mysql.createConnection(/*connectionString,*/{
         host:"localhost",
-        user:"root", // you can leave this or change it into any username that you set up
+        user:"root", // you can leave this or change it into any username taht you set up
         password:"password",// your database password
         database:"campus_eats_db"
     }).connect((err)=>{
@@ -17,49 +17,30 @@ function connectDB(callbackFunction){
             console.log(`--- Error: ${err}`)            
             console.log(`------------------------------------------------`)
         }else{
-
             callbackFunction();
         }});
 }
-// module.exports=connectDB;
 
 // --- other ways to connect to database using xdevapi
 // using a configuration object
 
-const config ={ 
-    host: 'localhost',
-    user:"root", // you can leave this or change it into any username taht you set up
-    password:"password",// your database password
-    database:"campus_eats_db",
-    port: 33060
-}
 function connectDBX(callbackFunction){
-    mysqlx.getSession(
-        config
-    ).catch(err => {
-        console.log(`-------------------- ERROR ---------------------`)
-        console.log(`- ${Date.now()} - Cannot connect to the Database`)
-        console.log(`--- Error: ${err}`)            
-        console.log(`------------------------------------------------`)
-    }).then(session => { //By default, the connector creates a new session using SSL/TLS for TCP connections.
-        console.log(`-> ${session.inspect().host}`); // { host: 'localhost', ssl: true }    
-        if(session){
-            console.log(session.port);
+    mysqlx.getSession({ 
+            host: 'localhost',
+            user:"root", // you can leave this or change it into any username taht you set up
+            password:"password",// your database password
+            database:"campus_eats_db"
+        }).then(session => { //By default, the connector creates a new session using SSL/TLS for TCP connections.
+            console.log(`-> ${session.inspect()}`); // { host: 'localhost', ssl: true }            
             callbackFunction();
-        }else{                
+        }).catch(err => {
             console.log(`-------------------- ERROR ---------------------`)
             console.log(`- ${Date.now()} - Cannot connect to the Database`)
-            console.log(`--- Error: ${session.path}`)            
+            console.log(`--- Error: ${err}`)            
             console.log(`------------------------------------------------`)
-        }
-    });
-        
-    // mysqlx.getSession('mysqlx://localhost?tls-versions=[TLSv1,TLSv1.1]')
-    // .catch(err => {
-    //     console.log(err); // TCP socket hang
-    // });
+        });
 }
-module.exports=connectDBX;
+module.exports=connectDB;
 
 // connection.connect()
 
