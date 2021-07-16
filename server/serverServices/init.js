b4e3664150ed8d2c0878496c6c4c06240da8c72c
d00415496 <d00415496@dmail.dixie.edu>
2021-07-16 12:45:25 -0600

    resolve conflict

--- server/serverServices/init.js
+++ server/serverServices/init.js
@@ -1,25 +1,13 @@
 const services = require("./services");
-<<<<<<< HEAD
 const dataAccess = require("../dataAccessLayer/DAO");
-=======
-const { connectDB, database } = require("../dataAccessLayer/DAO");
->>>>>>> reid
 
 // define a port
 const port = process.argv[2] || process.env.port || 7777;
 
-<<<<<<< HEAD
 //TODO:WRAP THIS WITH PROMISE WRAPPER: https://www.npmjs.com/package/mysql2#using-promise-wrapper
 dataAccess.connectDB(()=>{
     services.listen(port,()=>{
         console.log(`-> The CAMPUSEAT server is running at port:${port}`);
         console.log(`--------------------------------------------------`);
     });
-=======
-connectDB(() => {
-  services.listen(port, () => {
-    console.log(`-> The CAMPUSEAT server is running at port:${port}`);
-    console.log(`--------------------------------------------------`);
-  });
->>>>>>> reid
 });
