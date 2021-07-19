e3e0c3f5c850c1d5fe12edf0c2b2acb32687083b
minhducnguyen26 <minhducnguyen260101@gmail.com>
2021-07-19 08:32:42 -0600

    Test Vuex

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { store } from './store/store'
 
-createApp(App).use(router).mount('#app')
+new Vue({
+    el: "#app",
+    router,
+    store: store,
+    components: {
+        App
+    }
+})
