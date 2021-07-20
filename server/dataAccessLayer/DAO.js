2c3c414e2b7b7148408a0994226465e881489266
duydanghuynh <duyhdv@gmail.com>
2021-07-20 11:35:29 -0600

    update files and folder in DAO

--- server/dataAccessLayer/DAO.js
+++ server/dataAccessLayer/DAO.js
@@ -1,156 +1,21 @@
-// ============= Data Access Object ==============
-const mysql = require("mysql2");
-const path = require("path");
-const fs = require("fs");
-const databaseName = "campus_eats_db";
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password", // your root's password
-  // database: "campus_eats_db",
-});
+const mongoose = require("mongoose")
 
-function startupDatabase(callbackFunction){
-  connection.connect((err) => {
-    if (err) {
-      console.log(`-------------------- ERROR ---------------------`);
-      console.log(`- ${Date.now()} - Cannot connect to Mysql`);
-      console.log(`--- Error: ${err}`);
-      console.log(`------------------------------------------------`);
-    } else {
-      connection.prependListener
-      console.log(`-> Successfully connect to Mysql`);
-      console.log(`-> Trying to connect to database.`);
-      connection["database"]="campus_eats_db"
-      callbackFunction();
-    }
-  });
-}
-
-function connectDB(callbackFunction) {
-  console.log(`-> Trying to connect to Mysql server.`);
-  connection.connect((err) => {
-    if (err) {
-      console.log(`-------------------- ERROR ---------------------`);
-      console.log(`- ${Date.now()} - Cannot connect to Mysql`);
-      console.log(`--- Error: ${err}`);
-      console.log(`------------------------------------------------`);
-    } else {
-      console.log(`-> Successfully connect to Mysql`);
-      console.log(`-> Trying to connect to database.`);
-      //TODO: check if database have been created or not
-      //if database havent been created
-      if (!isExistDB(databaseName, connection)) {
-        console.log("database doesn't exist.")
-        let directory = path.join(
-          __dirname,
-          "mySqlQueries/initcampuseatsdb.sql"
-        );
-        //create database
-        if(createDatabase(directory,connection)){
-          //insert dummy data
-          let dummyFile = path.join(
-            __dirname,
-            "mySqlQueries/initCampusEatsData.sql"
-          );
-          insertDummyData(dummyFile,connection)
+function connectToDb(callback){
+    let connectionString="mongodb+srv://duy:P@ssw0rd@duydb.6mffl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    mongoose.connect(connectionString,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    }).catch(err=>{
+        if(err){
+            console.log(`-------------------- ERROR ---------------------`)
+            console.log(`- ${Date.now()} - Cannot connect to the Database`)
+            console.log(`--- Error: ${err}`)            
+            console.log(`------------------------------------------------`)
         }
-      } else {
-        console.log(`-> Successfully connect to database`);
-      }
-      callbackFunction();
-    }
-  })
+    });
+    mongoose.connection.once("open",callback);
 }
 
-// ================== HELPERS =====================
-
-//=Duy
-function isExistDB(databaseName, conn) {
-  let queryString = `SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '${databaseName}';`;
-  let isExisted = executeQuery(queryString, conn);
-  if ( isExisted!= Error) {
-    return true;
-  }
-  return false;
+module.exports = {
+    connectToDb
 }
-
-//=Duy
-function createDatabase(fileDirectory, connection){
-  let successfulCreatedDB = false;
-  while (successfulCreatedDB === false) {
-    successfulCreatedDB = executeQueriesFromFile(fileDirectory, connection);
-  }
-  if(successfulCreatedDB===true){
-    console.log(`-> Done Creating Database.`);
-  }else{
-    console.log(`-> FAILED Creating Database.`);
-  }
-  return successfulCreatedDB
-}
-
-//=Duy
-function insertDummyData(fileDirectory, connection){
-  let successInsert = false;
-  while (successInsert === false) {
-    successInsert = executeQueriesFromFile(fileDirectory, connection);
-  }
-  if(successInsert===true){
-    console.log(`-> Done Inserting Dummy Data.`);
-  }else{
-    console.log(`-> FAILED Inserting Dummy Data.`);
-  }
-  return successInsert
-}
-
-// =Duy
-//credit :https://www.thiscodeworks.com/javascript-import-sql-file-in-node-js-and-execute-against-postgresql-stack-overflow-sql-nodejs/5fc1488a5fb6ba00144ecb60
-function readAndSpiltQueries(fileDirectory) {
-  // Extract SQL queries from files. Assumes no ';' in the fileNames
-  let queries = fs
-    .readFileSync(fileDirectory)
-    .toString()
-    .replace(/(\r\n|\n|\r)/gm, " ") // remove newlines
-    .replace(/\s+/g, " ") // excess white space
-    .split(";") // split into all statements
-    .map(Function.prototype.call, String.prototype.trim)
-    .filter(function (el) {
-      return el.length != 0;
-    }); // remove any empty ones
-  return queries;
-}
-
-// =Duy
-function executeQuery(queryString, conn) {
-  conn.query(queryString, (err, results) => {
-    if (
-      err &&
-      err.code != "ER_TABLE_EXISTS_ERROR" &&
-      err.code != "ER_DB_CREATE_EXISTS"
-    ) {
-      console.log(`-------------------- ERROR ---------------------`);
-      console.log(`- ${Date.now()} - Cannot execute: ${queryString}`);
-      console.log(`--- ${err.code}`);
-      console.log(`------------------------------------------------`);
-      return err;
-    }
-    return results;
-  });
-}
-
-// =Duy
-function executeQueries(queries, conn) {
-  queries.forEach((query) => {
-    if(executeQuery(query, conn)===undefined){
-      return false
-    }
-  });
-  return true;
-}
-
-function executeQueriesFromFile(filePath,conn){
-  let queries = readAndSpiltQueries(filePath);
-  return executeQueries(queries,conn);
-}
-
-module.exports = { startupDatabase,connectDB, connection, executeQuery, readAndSpiltQueries };
