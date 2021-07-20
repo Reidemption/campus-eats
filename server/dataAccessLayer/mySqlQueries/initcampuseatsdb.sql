828c2f68be1dd09792226b4f90238cc347ce3963
duydanghuynh <duyhdv@gmail.com>
2021-07-20 10:50:09 -0600

    update DAP

--- server/dataAccessLayer/mySqlQueries/initcampuseatsdb.sql
+++ server/dataAccessLayer/mySqlQueries/initcampuseatsdb.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE campus_eats_db;
+CREATE DATABASE IF NOT EXISTS campus_eats_db;
 USE campus_eats_db;
-CREATE TABLE restaurants (
+CREATE TABLE IF NOT EXISTS restaurants (
 id int PRIMARY KEY auto_increment NOT NULL,
 name varchar(50) NOT NULL,
 description mediumtext,
@@ -8,7 +8,7 @@ location varchar(255) NOT NULL,
 image varchar(255) NOT NULL
 );
 
-CREATE TABLE menus (
+CREATE TABLE IF NOT EXISTS menus (
 id int PRIMARY KEY auto_increment NOT NULL,
 restaurant_id int,
 name varchar(50) NOT NULL,
@@ -17,7 +17,7 @@ image varchar(255),
 FOREIGN KEY (restaurant_id) REFERENCES restaurants(id)
 );
 
-CREATE TABLE meals (
+CREATE TABLE IF NOT EXISTS meals (
 id int PRIMARY KEY auto_increment NOT NULL,
 menu_id int,
 name varchar(50) NOT NULL,
@@ -28,13 +28,13 @@ calories varchar(15),
 FOREIGN KEY (menu_id) REFERENCES menus(id)
 );
 
-CREATE TABLE ingredients (
+CREATE TABLE IF NOT EXISTS ingredients (
 id int PRIMARY KEY auto_increment NOT NULL,
 name varchar(50) NOT NULL,
 description mediumtext
 );
 
-CREATE TABLE meals_ingredients (
+CREATE TABLE IF NOT EXISTS meals_ingredients (
 meal_id int NOT NULL,
 ingredient_id int NOT NULL,
 FOREIGN KEY (meal_id) REFERENCES meals(id),
@@ -42,12 +42,12 @@ FOREIGN KEY (ingredient_id) REFERENCES ingredients(id),
 PRIMARY KEY (meal_id, ingredient_id)
 );
 
-CREATE TABLE roles (
+CREATE TABLE IF NOT EXISTS roles (
 id int PRIMARY KEY auto_increment NOT NULL,
 name varchar(50) NOT NULL
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 id int PRIMARY KEY auto_increment NOT NULL,
 name varchar(50),
 role_id int NOT NULL,
@@ -58,15 +58,37 @@ image varchar(255),
 FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
-CREATE TABLE permissions (
+CREATE TABLE IF NOT EXISTS permissions (
 id int PRIMARY KEY auto_increment NOT NULL,
 name varchar(50)
 );
 
-CREATE TABLE roles_permissions (
+CREATE TABLE IF NOT EXISTS roles_permissions (
 role_id int NOT NULL,
 FOREIGN KEY (role_id) REFERENCES roles(id),
 permission_id int NOT NULL,
 FOREIGN KEY (permission_id) REFERENCES permissions(id),
 PRIMARY KEY (role_id, permission_id)
 );
+
+CREATE TABLE IF NOT EXISTS orders (
+id int PRIMARY KEY auto_increment NOT NULL,
+user_id INT NOT NULL,
+name varchar(50),
+total_price FLOAT NOT NULL,
+order_time DATETIME NOT NULL,
+is_delivered BIT NOT NULL,
+deliver_time DATETIME,
+FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS order_details (
+order_id int NOT NULL,
+meal_id int NOT NULL,
+partial_price FLOAT NOT NULL,
+order_time DATETIME NOT NULL,
+FOREIGN KEY (order_id) REFERENCES orders(id),
+FOREIGN KEY (meal_id) REFERENCES meals(id),
+PRIMARY KEY (order_id, meal_id)
+);
+
