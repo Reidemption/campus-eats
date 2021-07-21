09f6d99d70f428150dbdc2e98eb7b0f553f1c4f4
duydanghuynh <duyhdv@gmail.com>
2021-07-21 13:04:16 -0600

    add modules

---
+++ server/serverServices/businessLogic/models/data_models.js
@@ -0,0 +1,16 @@
+const User = require("./user");
+const UserInfo = require("./user_information");
+const Order = require("./order");
+const OrderItem = require("./order_item");
+const OrderItemSide = require("./order_item_side");
+const Menu = require("./menu");
+const Meal = require("./meal");
+const MealSide = require("./meal_side");
+const Category = require("./category");
+const Restaurant = require("./restaurant");
+
+module.exports={
+    User, UserInfo,
+    Order,OrderItem,OrderItemSide,
+    Restaurant, Category,Menu, Meal, MealSide
+}
