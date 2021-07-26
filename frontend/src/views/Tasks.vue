24ccbfefea76ef238cea014ec932f5286cce5c38
minhducnguyen26 <minhducnguyen260101@gmail.com>
2021-07-26 09:32:40 -0600

    Tasks page: in progress

--- frontend/src/views/Tasks.vue
+++ frontend/src/views/Tasks.vue
@@ -5,19 +5,32 @@
     </div>
 
     <div class="tasks_page_wrapper">
-      <MyComingSoon></MyComingSoon>
+      <div class="app_name">
+        <router-link to="/">Campus Eats</router-link>
+        <div class="additional_name">Delivery</div>
+      </div>
+
+      <div class="sort_orders_options">
+        <div class="title">Filter:</div>
+        <div class="dropdown_options">
+          <button class="dropdown_button">Select an option</button>
+          <div class="dropdown-content">
+          <p>Sort by near to far location</p>
+          <p>Sort by oldest to newest orders</p>
+          <p>Sort by </p>
+        </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import MyNavBar from "../components/MyNavBar.vue"
-import MyComingSoon from "../components/for_fun/MyComingSoon.vue"
 
 export default {
     components: {
         MyNavBar,
-        MyComingSoon
     }
 }   
 </script>
@@ -26,5 +39,34 @@ export default {
 .tasks_page_wrapper {
     height: 100vh;
     background-color: var(--gray);
+}
+
+.app_name {
+    padding: 30px 40px;
+    display: flex;
+    align-items: center;
+}
+
+.app_name > a {
+    font-family: 'Roboto Slab', serif;
+    font-size: 40px;
+    text-decoration: none;
+    color: var(--navy);
+    cursor: pointer;
+}
+
+.app_name> .additional_name {
+    font-family: 'Roboto Slab', serif;
+    font-size: 40px;
+    color: var(--red);
+    margin-left: 10px;
+}
+
+.sort_orders_options {
+    padding: 30px 40px;
+}
+
+.sort_orders_options > .title {
+    font-size: 20px;
 }
 </style>
